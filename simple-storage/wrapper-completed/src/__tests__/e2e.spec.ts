b661fefd603407f558ad24b62ff9066f6c8ecbfe
Kevin O'Connell <kevinoconnell42@gmail.com>
2022-03-07 23:53:47 -0600

    test initalized

---
+++ simple-storage/wrapper-completed/src/__tests__/e2e.spec.ts
@@ -0,0 +1,107 @@
+import {
+  buildAndDeployApi,
+  initTestEnvironment,
+  stopTestEnvironment,
+} from "@web3api/test-env-js";
+import { Web3ApiClient } from "@web3api/client-js";
+import path from "path";
+import { providers } from "ethers";
+
+import { getPlugins } from "./utils";
+
+jest.setTimeout(300000);
+
+describe("ENS Wrapper", () => {
+  // We will have two clients because we need two
+  // different signers in order to test ENS functions
+  let ownerClient: Web3ApiClient;
+  let anotherOwnerClient: Web3ApiClient;
+
+  let ensUri: string;
+  let ethersProvider: providers.JsonRpcProvider;
+  let ensAddress: string;
+  let registrarAddress: string;
+  let resolverAddress: string;
+  let reverseRegistryAddress: string;
+  let customFifsRegistrarAddress: string;
+  const SimpleStorageAddr = "0x0E696947A06550DEf604e82C26fd9E493e576337"
+  let owner: string;
+  let anotherOwner: string;
+
+  const customTld: string = "doe.eth";
+  const openSubdomain: string = "open." + customTld;
+  const customSubdomain: string = "john." + customTld;
+
+  const network: string = "testnet";
+
+  beforeAll(async () => {
+    const {
+      ensAddress: ensRegistryAddress,
+      registrarAddress: ensRegistrarAddress,
+      resolverAddress: ensResolverAddress,
+      reverseAddress: ensReverseAddress,
+      ipfs,
+      ethereum,
+    } = await initTestEnvironment();
+
+    // deploy api
+    const apiPath: string = path.resolve(__dirname + "/../../");
+    const api = await buildAndDeployApi(apiPath, ipfs, ensRegistryAddress);
+    ensUri = `ens/testnet/${api.ensDomain}`;
+
+    // set up ethers provider
+    ethersProvider = providers.getDefaultProvider(
+      "http://localhost:8546"
+    ) as providers.JsonRpcProvider;
+    owner = await ethersProvider.getSigner(0).getAddress();
+    anotherOwner = await ethersProvider.getSigner(1).getAddress();
+    ensAddress = ensRegistryAddress;
+    registrarAddress = ensRegistrarAddress;
+    resolverAddress = ensResolverAddress;
+    reverseRegistryAddress = ensReverseAddress;
+
+    // get client
+    const plugins = getPlugins(ethereum, ipfs, ensRegistryAddress);
+    ownerClient = new Web3ApiClient({ plugins });
+
+    const anotherOwnerRedirects = getPlugins(
+      ethereum,
+      ipfs,
+      ensRegistryAddress,
+      anotherOwner
+    );
+    anotherOwnerClient = new Web3ApiClient({ plugins: anotherOwnerRedirects });
+  });
+
+  afterAll(async () => {
+    await stopTestEnvironment();
+  });
+
+  it("should get ipfs data", async () => {
+    const variables = {
+      address: SimpleStorageAddr,
+      connection: {networkNameOrChainId: network},
+    };
+
+    const {
+      data: registerData,
+      errors: registerErrors,
+    } = await ownerClient.query<{
+      registerDomain: string;
+    }>({
+      uri: ensUri,
+      query: `query {
+       getIpfsData(
+         address: $address
+         connection: {
+           networkNameOrChainId: $network
+         }
+        )
+      }`,
+      variables,
+    });
+
+    expect(registerData?.registerDomain).toBeDefined();
+    expect(registerErrors).toBeUndefined();
+  });
+});
